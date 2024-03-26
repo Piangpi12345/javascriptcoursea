940b7f9a0f4c740d264df4cb317effcaf0654494
Piang Pi <piangpi2004119@gmail.com>
2024-03-26 20:58:08 +0630

    h3

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -9,10 +9,16 @@ function increaseCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
     }
 
-    function checkCountValue() {
-        if (count === 10) {
-          alert("Your Instagram post gained 10 followers! Congratulations!");
-        } else if (count === 20) {
-          alert("Your Instagram post gained 20 followers! Keep it up!");
-        }
-      }
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+function reset(){
+    count = 0;
+    document.getElementById('countDisplay').textContent=count;
+    alert ("Followers count has been reset");
+}
