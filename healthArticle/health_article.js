c2fa8011254fa52702fc0d13522bc3f4b7532ffc
Piang Pi <piangpi2004119@gmail.com>
2024-03-30 13:26:54 +0630

    h3

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 
-var url = './health_article.json';
+var url = 'http://localhost:8000/health_article.json';
 
 xhr.open('GET', url, true);
 
@@ -9,53 +9,47 @@ xhr.responseType = 'json';
 xhr.onload = function() {
     var articles = xhr.response.articles;
     var articlesDiv = document.getElementById('articles');
-}
 
-articles.forEach(function(article) {
-    var articleDiv = document.createElement('div');
-    articleDiv.classList.add('article');
+    articles.forEach(function(article) {
+        var articleDiv = document.createElement('div');
+        articleDiv.classList.add('article');
 
-    var title = document.createElement('h2');
-    title.textContent = article.title;
+        var title = document.createElement('h2');
+        title.textContent = article.title;
 
-    var description = document.createElement('p');
-    description.textContent = article.description;
+        var description = document.createElement('p');
+        description.textContent = article.description;
 
-    var waysHeader = document.createElement('h3');
-    waysHeader.textContent = 'Ways to Achieve:';
+        var waysHeader = document.createElement('h3');
+        waysHeader.textContent = 'Ways to Achieve:';
 
-    var waysList = document.createElement('ul');
-    article.ways_to_achieve.forEach(function(way) {
-      var listItem = document.createElement('li');
-      listItem.textContent = way;
-      waysList.appendChild(listItem);
+        var waysList = document.createElement('ul');
+        article.ways_to_achieve.forEach(function(way) {
+            var listItem = document.createElement('li');
+            listItem.textContent = way;
+            waysList.appendChild(listItem);
+        });
+
+        var benefitsHeader = document.createElement('h3');
+        benefitsHeader.textContent = 'Benefits:';
+
+        var benefitsList = document.createElement('ul');
+        article.benefits.forEach(function(benefit) {
+            var listItem = document.createElement('li');
+            listItem.textContent = benefit;
+            benefitsList.appendChild(listItem);
+        });
+
+        articleDiv.appendChild(title);
+        articleDiv.appendChild(description);
+        articleDiv.appendChild(waysHeader);
+        articleDiv.appendChild(waysList);
+        articleDiv.appendChild(benefitsHeader);
+        articleDiv.appendChild(benefitsList);
+
+        articlesDiv.appendChild(articleDiv);
     });
-
-    var benefitsHeader = document.createElement('h3');
-    benefitsHeader.textContent = 'Benefits:';
-
-    var benefitsList = document.createElement('ul');
-    article.benefits.forEach(function(benefit) {
-      var listItem = document.createElement('li');
-      listItem.textContent = benefit;
-      benefitsList.appendChild(listItem);
-    });
-
-    articleDiv.appendChild(title);
-    articleDiv.appendChild(description);
-    articleDiv.appendChild(waysHeader);
-    articleDiv.appendChild(waysList);
-    articleDiv.appendChild(benefitsHeader);
-    articleDiv.appendChild(benefitsList);
-
-    articlesDiv.appendChild(articleDiv);
-  });
-
-//   var articleDiv = document.createElement('div');
-
-//   articleDiv.classList.add('article');
-
-//   articleDiv.appendChild(title);
+};
 
   xhr.send();
 
